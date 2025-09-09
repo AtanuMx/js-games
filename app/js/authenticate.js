d5721de1bcdba74e39fa3cd51babd04dcff0f04b
AtanuMx <davidatanuinedu@gmail.com>
2025-09-09 17:59:24 +0100

    added a eye function

--- app/js/authenticate.js
+++ app/js/authenticate.js
@@ -1,12 +1,29 @@
 let validateInput = (event) => {
     let inputElement = event.target;
-    let errorElement = inputElement.nextElementSibling;
+    // let errorElement = inputElement.nextElementSibling;
+
+    // console.log(errorElement.tagName);
+
 
     if (inputElement.value.length !== 0 && inputElement.value.length < 6) {
-        inputElement.style.backgroundColor = 'red';
-        errorElement.innerText = "password too week"
+        inputElement.style.backgroundColor = '#ffebd9';
+        // errorElement.innerText = "password too week"
     } else {
-        errorElement.innerText = ""
+        // errorElement.innerText = ""
         inputElement.style.backgroundColor = 'transparent';
     }
+}
+
+let toggleInput = (elementId) => {
+    let inputElement = document.getElementById(elementId, event);
+    let button = event.target.tagName == 'BUTTON' ? event.target : event.target.parentElement;
+
+    if (inputElement.getAttribute('type') === 'password') {
+        inputElement.setAttribute('type', 'text');
+        button.innerHTML = '<i class="far fa-eye-slash"></i>';
+    } else {
+        inputElement.setAttribute('type', 'password');
+        button.innerHTML = '<i class="far fa-eye"></i>';
+    }
+
 }
